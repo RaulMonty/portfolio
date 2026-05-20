ef7d275e98b24060967ee039341796c4f01a80f1
Raul Montiel <ramontyfe@hotmail.com>
2026-05-20 17:15:44 +0200

    Fix Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './App.css'
 import App from './App'
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
